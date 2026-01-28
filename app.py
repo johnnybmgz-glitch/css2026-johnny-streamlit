1e855caf234dc387588b722d8af0b71c983779cd
johnnybmgz-glitch <johnnybmgz@gmail.com>
2026-01-28 20:31:34 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 28 18:44:34 2026
+
+@author: User
+"""
+import streamlit as st
+
+st.set_page_config(
+    page_title="Johnny – CSS 2026",
+    page_icon="👨🏾‍💻"
+)
+
+st.title("Johnny Lukanka")
+st.subheader("Coding Summer School 2026")
+
+st.write("""
+This is my public Streamlit website.
+It presents my profile and my work.
+""")
+
+st.header("📌 About Me")
+st.write("""
+I am a student in the Coding Summer School.
+I am learning Python and Streamlit.
+""")
+
