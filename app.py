103522c990a4c062ce72894ef9b1f7a25ddb2e32
johnnybmgz-glitch <johnnybmgz@gmail.com>
2026-02-01 11:30:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,27 +15,42 @@ st.set_page_config(
 
 st.title("Johnny Lukanka")
 st.subheader("Coding Summer School 2026")
-
+st.subheader("Whatever you do, as long as you choose to do it, dedicate your heart")
 st.write("""
 This is my public Streamlit website.
 It presents my profile and my work.
 """)
 
-st.header("📌 About Me")
+st.header("About Me")
 st.write("""
-I am a student in the Coding Summer School.
-I am learning Python and Streamlit.
+My name is Johnny, and I am passionate about technology and continuous learning.
+I enjoy creating, testing, fixing, and starting again until it works and I understand why.
+
+Through my projects, I am building skills in programming, data, 
+and website development, with a practical and progressive approach. 
+This website reflects my journey, my efforts, and my desire to turn 
+learning into something concrete and personal.
 """)
-st.subheader("🚀 What I'm working on")
+st.subheader("I'm working on")
 st.markdown("""
 - Learning Python fundamentals  
 - Building data pipelines  
 - Creating Streamlit web apps
-- I love beans so bad
 """)
-st.image("car3.jpg", width=2200)
-st.image("car7.jpg", width=2200)
-st.image("car11.jpg", width=2200)
+st.image("Landcruiser1.jpeg", width=2200)
+st.markdown("""
+This is not the best car in the world, but it's my favourite, the first one! Toyota LandCruiser
+""")
+
+st.image("Ford1.avif", width=2200)
+st.markdown("""
+This is my second one, a Ford Ranger
+""")
+
+st.image("DodgeRam.jpg", width=2200)
+st.markdown("""
+This is my third one, a DodgeRam
+""")
 
 
 
